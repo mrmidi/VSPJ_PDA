33b2f02406fed704a2e51576f40f7481de8e265a
Aleksandr Shabelnikov <ashabeln@redhat.com>
2022-09-30 15:51:25 +0200

    Part 3 added

---
+++ CV01/PART_03/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PART_03;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ CV01/PART_03/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace PART_03;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    void ShowPasswords(System.Object sender, Microsoft.Maui.Controls.ToggledEventArgs e)
+    {
+		if (ShowPwSwitch.IsToggled)
+		{
+            pw1.IsPassword = false;
+            pw2.IsPassword = false;
+        }
+		else
+		{
+            pw1.IsPassword = true;
+            pw2.IsPassword = true;
+        }
+    }
+
+    private async void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+		if (pw1.Text == pw2.Text)
+		{
+            //await DisplayAlert("1", "2", "OK");
+            await DisplayAlert("Password", "Your password is fine", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Password", "Passwords doen't match", "OK");
+        }
+    }
+}
+
+
